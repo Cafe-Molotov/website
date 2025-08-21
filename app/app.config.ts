531162384aac506773654cc129499c25329908ab
Sander Geyssens <sander.xsg@gmail.com>
2025-08-21 02:08:07 +0200

    set colours

---
+++ app/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'red',
+      secondary: 'rose',
+      success: 'green',
+      info: 'blue',
+      warning: 'yellow',
+      error: 'red',
+      neutral: 'neutral'
+    }
+  }
+})
