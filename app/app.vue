e7b9ed8daea1e17eb85fcb0c89abce4243875863
Sander Geyssens <sander.xsg@gmail.com>
2025-08-21 02:08:24 +0200

    add navigation

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,58 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const navItems = ref<NavigationMenuItem[]>([
+  {
+    label: 'Home',
+    icon: 'i-lucide-house',
+    to: '/',
+    children: [
+      {
+        label: 'Over ons',
+        icon: 'el:group',
+        to: '/#about'
+      },
+      {
+        label: 'Dranken',
+        icon: 'lucide-lab:wine-glass-bottle',
+        to: '/#drinks'
+      },
+    ]
+  },
+  {
+    label: 'News',
+    icon: 'i-lucide-book-open-text',
+    to: '/news',
+    children: [
+      {
+        // Ideally this would be filled up dynamically with the latest few posts.
+
+        label: 'Latest post',
+        icon: 'i-lucide-file-text',
+        description: 'This description is just an example.',
+        to: '/'
+      },
+      {
+        label: 'Molotov krant',
+        icon: 'i-lucide-file-text',
+        to: '/'
+      }
+    ]
+  },
+  {
+    label: 'Events',
+    icon: 'i-lucide-calendar',
+    to: '/events'
+  }
+
+  // Social media
+  // Language
+])
+</script>
+
 <template>
   <UApp>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UNavigationMenu content-orientation="vertical" :items="navItems" class="w-full justify-center" />
+    <NuxtPage />
   </UApp>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>hey index</h1>
+</template>
+
+<style lang="scss">
+
+</style>
