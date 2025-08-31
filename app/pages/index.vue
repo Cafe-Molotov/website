d88feabe7ae5a9b9c75c30f15ea9a7f276d2514a
Sander Geyssens <sander.xsg@gmail.com>
2025-08-31 02:02:35 +0200

    add content to home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,6 +12,7 @@ const carouselItems = [
 <template>
     <UContainer>
         <div>
+            <h1 class="text-4xl font-bold">Eigenzinnig cafeetje met optredens, tuinterras en democratische prijzen</h1>
             <NuxtImg src="hero.jpg" class="rounded-lg" />
         </div>
         
@@ -21,7 +22,33 @@ const carouselItems = [
         
         <USeparator class="h-16"/>
 
+            <h2 id="about" class="text-3xl font-bold">Over ons</h2>
+            
+            <div class="md:flex gap-16">
+                <div>
+                    <p class="mt-4 text-lg text-jusify">
+                        Welkom bij Molotov, waar elke avond een avontuur wacht om zich te ontvouwen.
+                        Gelegen in het hart van Gent zijn we niet zomaar een bar.
+                        We zijn een toevluchtsoord voor mensen die op zoek zijn naar iets anders.
+                    </p>
+
+                    <p class="mt-4 text-lg text-jusify">
+                        Wij zijn van mening dat aan goede tijden geen hoog prijskaartje mag hangen.
+                        Daarom zijn we er trots op dat we democratische prijzen kunnen aanbieden die
+                        kwaliteitsdranken en entertainment voor iedereen toegankelijk maken.
+                        Of je nu een student met een beperkt budget bent of een doorgewinterde connaisseur,
+                        op onze drankkaart vind je iets om van te houden zonder veel geld uit te geven.
+                    </p>
+                </div>
+
+                <div class="w-full">
+                    <NuxtImg src="vroeger.jpg" class="mt-4 rounded-xs" />
+                </div>
+            </div>
+
         <USeparator class="h-16" />
+
+            <h2 id="drinks">Dranken</h2>
     </UContainer>
 </template>
 
